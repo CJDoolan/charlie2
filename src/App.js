7a3edc4c05edcdec2140fc9e7226c347314d9e9c
James Dong <jamesdong621@gmail.com>
2024-12-03 11:08:42 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Home'; // 引入 Home 组件
+
+function App() {
+    return (
+        <div>
+            <Home /> {/* 渲染 Home 组件 */}
+        </div>
+    );
+}
+
+export default App; 
